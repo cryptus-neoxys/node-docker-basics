7ad9375c7ae76758aa78886402019ecb57e85321
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-05-06 16:51:14 +0530

    minor fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-  return res.status(200).json({ messge: "Hey there" });
+  return res.status(200).json({ messge: "Hey there!!!" });
 });
 
 app.listen(PORT, () => console.log(`🚀 server listening on ${PORT}`));
