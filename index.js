2d1a092ff17f0872bbd240ef37fee68d9915ac6a
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-05-09 04:37:44 +0530

    ➕ Add mongoose

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-  return res.status(200).json({ messge: "Hey there" });
+  return res.status(200).json({ messge: "Hey there!" });
 });
 
-app.listen(PORT, () => console.log(`🚀 server listening on ${PORT}`));
+app.listen(PORT, () => {
+  mongoose
+    .connect("mongodb://root:root@mongo/?authSource=admin", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.error(err));
+  console.log(`🚀 server listening on ${PORT}`);
+});
