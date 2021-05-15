63c802a8da3e28abbdb128d2a8739c8934641c61
Dev Sharma <sharma.dev4242@gmail.com>
2021-05-16 05:08:33 +0530

    ✅ user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,8 @@ const User = require("../models/userModel");
 
 exports.signUp = async (req, res) => {
   const { username, password } = req.body;
-  const hashPassword = await bcrypt.hash(password, 12);
   try {
+    const hashPassword = await bcrypt.hash(password, 12);
     const user = await User.create({
       username,
       password: hashPassword,
@@ -16,6 +16,37 @@ exports.signUp = async (req, res) => {
       data: {
         user,
       },
+    });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(400).json({
+      status: "fail",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "user not found",
+      });
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+      return res.status(201).json({
+        status: "success",
+      });
+    }
+
+    return res.status(403).json({
+      status: "fail",
+      message: "incorrect username or password",
     });
   } catch (err) {
     console.error(err);
