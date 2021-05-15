670e1604aa41c8f37796a2243bdc9a61757f709a
Dev Sharma <sharma.dev4242@gmail.com>
2021-05-16 05:26:26 +0530

    Add sessions with redis container

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const { json } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
+const redis = require("redis");
+const session = require("express-session");
+let RedisStore = require("connect-redis")(session);
+
 const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET,
 } = require("./config/config");
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
 
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
+
 const app = express();
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 15000,
+    },
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
